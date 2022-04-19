df6b7be7ea876405d600c38377fcf97af41c1257
Sij <sijibamgboye@yahoo.com>
2022-04-19 18:58:31 +0100

    recursive function that calls function until the list is resolved

---
+++ recursive-binary-search.py
@@ -0,0 +1,27 @@
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint= (len(list)) //2
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at: " , index)
+    else:
+        print("Target not in the list") 
+
+
+numbers= [0,3,4,5,5]
+
+result = recursive_binary_search(numbers, 5)
+
+verify(result)
