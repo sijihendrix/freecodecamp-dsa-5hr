bea59e5eb18cb8f861b8bff4d801072a9f3d75bd
Sij <sijibamgboye@yahoo.com>
2022-04-19 18:37:45 +0100

    binary search with while loop. search for value at midpoint and reduce arr based on result of value

--- binary-search.py
+++ binary-search.py
@@ -1,2 +1,13 @@
-def binary_search():
+def binary_search(list, target):
+    first, last = 0, len(list) - 1
+
+    while first <= last:
+        midpoint = first + last // 2
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last =  midpoint - 1
+    return  None
     pass
