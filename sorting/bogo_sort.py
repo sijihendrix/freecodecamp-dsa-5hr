dde84cf3df5896a682173df065aafc427e1813c5
Sij <sijibamgboye@yahoo.com>
2022-05-02 19:48:50 +0100

    bogo sort and selection sort

--- sorting/bogo_sort.py
+++ sorting/bogo_sort.py
@@ -5,4 +5,18 @@ from load import load_numbers
 
 numbers = load_numbers(sys.argv[1])
 
-print(numbers)
+# print(numbers)
+
+def is_sorted(values):
+    for i in range(len(values) - 1):
+        if values[i] > values[i + 1]:
+            return False
+    return True
+
+
+def bogo_sort(values):
+    while not is_sorted(values):
+        random.shuffle(values)
+    return values
+
+print(bogo_sort(numbers))
